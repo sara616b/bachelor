a73b4fcc79487c2bbe1ee0e0d14a638d9ac7271f
Sarah Frederiksen <sara616b@stud.kea.dk>
2023-04-29 11:41:09 +0200

    added tailwind and started styling on website

--- project/frontend/website/Modules/Column.jsx
+++ project/frontend/website/Modules/Column.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import classnames from 'classnames';
 
-const Column = ({ column, children }) => {
+const Column = ({ column, index, wrap, alignContent, children }) => {
+
+    const columnClassnames = classnames(
+        "w-full",
+        "grid",
+        {
+            "place-content-center": alignContent == "center",
+            "place-content-end": alignContent == "right",
+            "place-content-start": alignContent == "left",
+            "order-1": index == 2 && wrap == "reverse",
+            "order-2": index == 1 && wrap == "reverse",
+            "md:order-1": index == 1,
+            "md:order-2": index == 2,
+        }
+    )
+
     return (
-        <div>
+        <div className={columnClassnames}>
             {React.Children.map(children, child => { return child })}
         </div>
     )
